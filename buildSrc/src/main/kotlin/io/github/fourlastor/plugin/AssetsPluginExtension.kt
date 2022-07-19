717f5bd8a52100bbda69fd09f25bd4acf0d872c4
Daniele Conti <fourlastor@users.noreply.github.com>
2022-07-19 11:12:44 +0100

    Remove unused import

--- buildSrc/src/main/kotlin/io/github/fourlastor/plugin/AssetsPluginExtension.kt
+++ buildSrc/src/main/kotlin/io/github/fourlastor/plugin/AssetsPluginExtension.kt
@@ -3,7 +3,6 @@ package io.github.fourlastor.plugin
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import org.gradle.api.provider.Property
-import java.io.File
 
 open class AssetsPluginExtension(project: Project) {
     /**
